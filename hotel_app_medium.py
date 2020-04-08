90f5893069d2f5b31f26d3eedae338b2b030927e
NBESS <nicholasdbess@gmail.com>
2020-04-07 22:44:26 -0400

    starts building the medium app

---
+++ hotel_app_medium.py
@@ -0,0 +1,29 @@
+# First, create a list of hotel dictionaries. 
+# They should contain at least 3 identical dictionaries (like the one in the example above).
+
+# Create a "while True" loop to show the following menu:
+# 1. Print hotel room status
+# 2. Check in customer
+# 3. Check out customer
+# 4. Quit
+
+
+# - When printing, show all rooms for all hotels and the name of the occupant, if there is one.
+# - When checking a customer in, make sure to choose a hotel as well as a room.
+# - Do not let a customer check into an occupied room.
+# - If the room is unoccupied, prompt for each piece of information (name, cell, etc.)
+# - upon check out, print out whether or not the customer has paid
+
+main_menu = '''
+
+1. Print hotel room status
+2. Check in customer
+3. Check out customer
+4. Quit
+
+'''
+
+while True:
+    print(main_menu)
+
+
